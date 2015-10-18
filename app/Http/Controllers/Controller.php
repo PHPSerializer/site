c8066bf4b3e96dafaccf835a2a4a890af2961d82
Nil Portugués Calderó <nilopc@gmail.com>
2015-10-18 14:25:51 +0200

    docs homepage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,13 +6,25 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @return \Illuminate\View\View
+     */
     public function indexAction()
     {
         return view('pages/home');
     }
 
+    /**
+     * @return \Illuminate\View\View
+     */
     public function documentationAction()
     {
-        return view('pages/documentation');
+        return view
+        (
+            'pages/documentation',
+            [
+                'navbar' => 'docs'
+            ]
+        );
     }
 }
